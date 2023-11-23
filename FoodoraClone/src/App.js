5148853332ab31d237f05d9dd8ce8adba8d46795
edvinvukas <144057889+edvinvukas@users.noreply.github.com>
2023-11-23 11:21:33 +0100

    ändringar git pull ¨

--- FoodoraClone/src/App.js
+++ FoodoraClone/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import BrowsingSection from './components/BrowsingPage';
+import RestaurantItems from './restarauntMenu/restaurant';
 
 
 function App() {
--- FoodoraClone/src/restarauntMenu/restaurant.js
+++ FoodoraClone/src/restarauntMenu/restaurant.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Menu from "./menu";
-import logo from "./logo.svg";
+import logo from "../logo.svg";
 
         const RestaurantItems = [
             {id: 1, name: 'Kalles korvkiosk', image:{logo},  ingredients: ['Korv', 'Potatismos']},
             {id: 2, name: 'Pelles peniskorv', image:{logo}, ingredients: ['Nötkött', 'Sallad', 'Lök']}
         ];
         
-        return(
-            <div>
-            <Menu items = {menuItems} />
-            </div>
+        const Restaurant = () => {
+            return(
+                <div>
+                    RestaurantItems
+                    <Menu items = {RestaurantItems} />
+                </div>
             );
+        }
+export default Restaurant;
