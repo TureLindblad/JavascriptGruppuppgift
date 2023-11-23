eacefbe0b725516b5188ca94f2e428d4ceb0a77f
edvinvukas <144057889+edvinvukas@users.noreply.github.com>
2023-11-23 11:33:04 +0100

    OCD

--- FoodoraClone/src/restarauntMenu/restaurant.js
+++ FoodoraClone/src/restarauntMenu/restaurant.js
@@ -1,18 +1,9 @@
-import React from "react";
-import Menu from "./menu";
+
 import logo from "../logo.svg";
 
-        const RestaurantItems = [
-            {id: 1, name: 'Kalles korvkiosk', image:{logo},  ingredients: ['Korv', 'Potatismos']},
-            {id: 2, name: 'Pelles peniskorv', image:{logo}, ingredients: ['Nötkött', 'Sallad', 'Lök']}
-        ];
-        
-        const Restaurant = () => {
-            return(
-                <div>
-                    RestaurantItems
-                    <Menu items = {RestaurantItems} />
-                </div>
-            );
-        }
-export default Restaurant;
+const RestaurantItems = [
+    {id: 1, name: 'Kalles korvkiosk', image:{logo},  ingredients: ['Korv', 'Potatismos'], category: ['Hamburgare', 'Kebab']},
+    {id: 2, name: 'Pelles peniskorv', image:{logo}, ingredients: ['Nötkött', 'Sallad', 'Lök'], category: ['Sushi', 'Sallad']}
+];
+
+export default RestaurantItems;
