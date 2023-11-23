fb4d562a33c4e4557ea0475c36b55ddbd2ce16cf
Ivar Arnesson <ivararnesson@Ivars-MacBook-Pro.local>
2023-11-23 11:27:40 +0100

    startsida

--- FoodoraClone/src/App.js
+++ FoodoraClone/src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Login } from './Login';
+import { Register } from './Register';
 import './App.css';
 
+const App = () => {
+  const [currentForm, setCurrentForm] = useState('');
 
-function App() {
-  
+  const toggleForm = (formName) => {
+      setCurrentForm(formName);
+  }
+
   return (
-    <div className="App">
-      Hello, world!
+    <div className='container'>
+        {
+          currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+        }
     </div>
-    
   );
-}
+};
 
 export default App;
+
+
