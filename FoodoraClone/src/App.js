4195af5f18e86ae21292dced447a15913e3fd26d
Paul Hertz <paularam22@gmail.com>
2023-11-24 08:59:58 +0100

    la in design för knappar och sökfält

--- FoodoraClone/src/App.js
+++ FoodoraClone/src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  
   return (
-    <div className="App">
-      Hello, world!
+    <div className="container">
+      <input
+        className="search"
+        type="search"
+        placeholder="Sök något..."
+      ></input>
+      <button className="btn" type="button">
+        Knapp
+      </button>
+
+      <button className="btn-arrow">
+        <i class="arrow right"></i>
+      </button>
+      <button className="btn-arrow">
+        <i class="arrow left"></i>
+      </button>
+      <button className="btn-arrow">
+        <i class="arrow up"></i>
+      </button>
+      <button className="btn-arrow">
+        <i class="arrow down"></i>
+      </button>
     </div>
-    
   );
 }
 
