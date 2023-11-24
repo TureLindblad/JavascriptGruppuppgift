02c872f10e6a7e6c6439dad65de9ac285cc6935a
TureLindbladLaptop <ture.gustaf.lindblad@gmail.com>
2023-11-24 13:26:49 +0100

    add navbar branch

--- FoodoraClone/src/App.js
+++ FoodoraClone/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from "./navbar.js"
 
 function App() {
   
   return (
     <div className="App">
-      Hello, world!
+      <Navbar />
     </div>
     
   );
---
+++ FoodoraClone/src/navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <p>ett</p>
+                </li>
+                <li>
+                    <p>två</p>
+                </li>
+            </ul>
+        </nav>
+    );
+}
