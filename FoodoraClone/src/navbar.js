2d05ff859287c3c5715b207a214e9cb335457943
TureLindbladLaptop <ture.gustaf.lindblad@gmail.com>
2023-11-27 09:26:13 +0100

    navbar basic

--- FoodoraClone/src/navbar.js
+++ FoodoraClone/src/navbar.js
@@ -1,14 +1,27 @@
 export default function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <p>ett</p>
-                </li>
-                <li>
-                    <p>två</p>
-                </li>
-            </ul>
-        </nav>
+        <header>
+            <nav className="navbar">
+                <NavbarList />
+            </nav>
+        </header>
+    );
+}
+
+function NavbarList() {
+    return (
+        <ul className="navbarList">
+            <NavbarListItem textContent={"Mat och Restauranger"} />
+            <NavbarListItem textContent={"grejer"} />
+            <NavbarListItem textContent={"tre"} />
+        </ul>
+    );
+}
+
+function NavbarListItem({ textContent }) {
+    return (
+        <li className="navbarListItem">
+            <p>{textContent}</p>
+        </li>
     );
 }
